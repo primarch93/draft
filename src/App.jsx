345e66d4e393967fd48b2788893503cc76cd0bb6
primarch93 <sugonegor@gmail.com>
2023-04-18 23:50:44 +0300

    clean-code

--- src/App.jsx
+++ src/App.jsx
@@ -3,63 +3,25 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-   count: 0,
-   iscounting: false
+  posts: [
+    {id: 'a1', name: 'JS Bassic'},
+    {id: 'a2', name: 'JS Advanced'},
+    {id: 'a3', name: 'React JS'},
+  ],
   };
   
 
-  
-componentDidMount() {
-  const usercount = localStorage.getItem("timez");
-  if (usercount) {
-    this.setState({count: +usercount});
-  }
-}
- 
 
 
-componentDidUpdate() {
-    localStorage.setItem("timez", this.state.count)
-  }
-
-
-  componentWillUnmount(){
-    clearInterval(this.counterId)
-}
-
-
-HandleStart = () => {
-  this.setState({iscounting: true})
-
-  this.counterId = setInterval(() => {
-    this.setState({count: this.state.count + 1})
-  }, 1000);
-};
-
-handleReset = () => {
-  this.setState({iscounting: false, count: 0});
-  clearInterval(this.counterId)
-};
-
-handleStop = () => {
-  this.setState({iscounting: false});
-  clearInterval(this.counterId)
-};
   render () {
     return (
     <div className="App">
-     <h1>Timer-React</h1>
-     <h3>{this.state.count}</h3>
-     {!this.state.iscounting ? (
-      <button onClick={this.HandleStart}>Start</button>
-     ) : (
-      <button onClick={this.handleStop}>Stop</button>
-     )}
-     <button onClick={this.handleReset}>Reset</button>
+  {this.state.posts.map((post) => (
+    <h2 key={post.id}>{post.name}</h2>
+  ))}
     </div>
     );
-}
-}
-
+    }
+  }
 
 export default App;
