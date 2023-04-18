769299451dbef180986971de35095945afbbde07
primarch93 <sugonegor@gmail.com>
2023-04-18 22:59:22 +0300

    custom- timer

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,59 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    posts:[],
-    loading:true,
-    comments:[],
+   count: 0,
+   iscounting: false
   };
   
-componentDidMount() {
-  console.log("componentDidMount");
-  fetch('https://jsonplaceholder.typicode.com/posts')
-.then(response => response.json())
-.then(data => this.setState({posts:data, loading: false}))
 
-this.timeId = setInterval(() => {
-  // this.setState({loading: true})
-  fetch('https://jsonplaceholder.typicode.com/comments')
-  .then(response => response.json())
-  .then(data => this.setState({comments:data}))
-}, 3000)
-}
-  componentDidUpdate() {
-    console.log("componentDidUpdate");
+  
+componentDidMount() {
+  const usercount = localStorage.getItem("timez");
+  if (usercount) {
+    this.setState({count: +usercount});
   }
-componentWillUnmount(){
-  clearInterval(this.timeId);
 }
-// handleClick() {
-//   this.setState({count: this.state.count + 1});
-//   // this.setState((prevState) => ({count:prevState.count + 1}), () => {
-//   //   console.log('setState complate')
-//   // });
-//   // this.setState((prevState) => ({count:prevState.count + 1}));
-//   // this.setState((prevState) => ({count:prevState.count + 1}));
-// console.log('from handleClick');
-// }
+ 
+
+
+componentDidUpdate() {
+    localStorage.setItem("timez", this.state.count)
+  }
+
+
+  componentWillUnmount(){
+    clearInterval(this.counterId)
+}
+
+
+HandleStart = () => {
+  this.setState({iscounting: true})
+
+  this.counterId = setInterval(() => {
+    this.setState({count: this.state.count + 1})
+  }, 1000);
+};
+
+handleReset = () => {
+  this.setState({iscounting: false, count: 0});
+  clearInterval(this.counterId)
+};
+
+handleStop = () => {
+  this.setState({iscounting: false});
+  clearInterval(this.counterId)
+};
   render () {
     return (
     <div className="App">
-     {this.state.loading ? <h3>Loading...</h3> : <h3>
-      {this.state.posts.length} was loading
-      </h3>}
+     <h1>Timer-React</h1>
+     <h3>{this.state.count}</h3>
+     {!this.state.iscounting ? (
+      <button onClick={this.HandleStart}>Start</button>
+     ) : (
+      <button onClick={this.handleStop}>Stop</button>
+     )}
+     <button onClick={this.handleReset}>Reset</button>
     </div>
     );
 }
