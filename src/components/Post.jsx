c6f10c1e055fc2a98499ebdf8c2921420f640fc2
primarch93 <sugonegor@gmail.com>
2023-04-20 17:48:34 +0300

    task-5

--- src/components/Post.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function Post (props) {
-const{id, name, removePost} = props;
-
-    return <h2>{name} <button onClick={() => removePost(id)}>delete</button></h2>
-}
